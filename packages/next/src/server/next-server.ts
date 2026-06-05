52dfd8270be0c9ecbff692344fe1561e8b0e9989
Tim Neutkens <tim@timneutkens.nl>
2026-06-05 20:11:03 +0200

    Document direct server match fallbacks

--- packages/next/src/server/next-server.ts
+++ packages/next/src/server/next-server.ts
@@ -1121,6 +1121,8 @@ export default class NextNodeServer extends BaseServer<
 
       let match = getRequestMeta(req, 'match')
 
+      // router-server attaches the fsChecker match, but standalone custom
+      // servers and other direct BaseServer entrypoints bypass that handoff.
       if (!match) {
         const localeAnalysisResult = this.i18nProvider?.analyze(pathname, {
           defaultLocale: getRequestMeta(req, 'defaultLocale'),
@@ -1132,7 +1134,8 @@ export default class NextNodeServer extends BaseServer<
         }
       }
 
-      // If we don't have a match, try to render it anyways.
+      // Direct request handlers can receive arbitrary paths that have no route
+      // definition. Let the renderer produce the appropriate 404 response.
       if (!match) {
         await this.render(req, res, pathname, query, parsedUrl, true)
 
