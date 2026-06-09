acacb84ccf8362a0dc12d9ddac0628952dcd9942
Tim Neutkens <tim@timneutkens.nl>
2026-06-09 12:56:11 +0200

    Replace server fields with typed dev state

--- packages/next/src/server/next-server.ts
+++ packages/next/src/server/next-server.ts
@@ -133,6 +133,7 @@ import {
 } from './lib/router-utils/router-server-context'
 import { installGlobalBehaviors } from './node-environment-extensions/global-behaviors'
 import { installProcessErrorHandlers } from './node-environment-extensions/process-error-handlers'
+import type { DevServerStateUpdate } from './dev/dev-server-state'
 
 export * from './base-server'
 
@@ -188,10 +189,7 @@ export default class NextNodeServer extends BaseServer<
     page: string
     re: RegExp
   }[]
-  private routerServerHandler?: (
-    req: IncomingMessage,
-    res: ServerResponse
-  ) => void
+  private routerServerHandler: Options['routerServerHandler']
 
   protected cleanupListeners = new AsyncCallbackSet()
   protected internalWaitUntil: WaitUntil | undefined
@@ -202,6 +200,7 @@ export default class NextNodeServer extends BaseServer<
     // Initialize super class
     super(options)
 
+    this.routerServerHandler = options.routerServerHandler
     installGlobalBehaviors(this.nextConfig)
 
     // Load prefetch hints from the build output. This must happen before
@@ -344,6 +343,16 @@ export default class NextNodeServer extends BaseServer<
         // Intentionally ignored because this is a preload step.
       }
     }
+  }
+
+  /** @internal */
+  public updateDevServerState(_update: DevServerStateUpdate): void {
+    throw new Error('Invariant: dev server state can only be updated in dev')
+  }
+
+  /** @internal */
+  public reloadEnv(): void {
+    this.loadEnvConfig({ dev: true, forceReload: true })
   }
 
   protected async handleUpgrade(): Promise<void> {
